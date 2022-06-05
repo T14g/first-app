ccf8f6bf80ed48a8d258ac241931292abadcacdc
Tiago <ottiagos@gmail.com>
2022-06-05 01:03:29 -0300

    Creating a button with Button primitive

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet,View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello world 123</Text>;
+  return (
+    <View>
+       <Text style={styles.text}>Hello world 123</Text>
+       <Button title="Go to components Demo"/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
