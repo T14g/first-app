788cde964ef86fade54de8280e8f33509761874e
Tiago <ottiagos@gmail.com>
2022-06-06 12:41:45 -0300

    Creating and using a child component passing props

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-    return <Text>Image Screen</Text>;
+    return (
+        <View>
+            <Text>Image Screen</Text>
+            <ImageDetail title={'Image 1'}/>
+            <ImageDetail title={'Image 2'}/>
+            <ImageDetail title={'Image 3'}/>
+            <ImageDetail title={'Image 4'}/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
