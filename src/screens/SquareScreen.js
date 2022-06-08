2f2d925a77d7f46b533f45da5997d9603d9ac136
Tiago <ottiagos@gmail.com>
2022-06-08 20:16:31 -0300

    Creating a color changing square based on rgb changes

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+// True constant
+const COLOR_INCREMENT = 10;
+
 const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [blue, setBlue] = useState(0);
@@ -9,13 +12,21 @@ const SquareScreen = () => {
 
     return (
         <View>
-            <ColorCounter color={'Red'} onIncrease={() => setRed(red + 1)} onDecrease={() => setRed(red - 1)} />
-            <ColorCounter color={'Blue'} onIncrease={() => setBlue(blue + 1)} onDecrease={() => setBlue(blue - 1)} />
-            <ColorCounter color={'Green'} onIncrease={() => setGreen(green + 1)} onDecrease={() => setGreen(green - 1)} />
+            <ColorCounter color={'Red'} onIncrease={() => setRed(red + COLOR_INCREMENT)} onDecrease={() => setRed(red - COLOR_INCREMENT)} />
+            <ColorCounter color={'Blue'} onIncrease={() => setBlue(blue + COLOR_INCREMENT)} onDecrease={() => setBlue(blue - COLOR_INCREMENT)} />
+            <ColorCounter color={'Green'} onIncrease={() => setGreen(green + COLOR_INCREMENT)} onDecrease={() => setGreen(green - COLOR_INCREMENT)} />
 
             <Text>Red : {red}</Text>
             <Text>Blue : {blue}</Text>
             <Text>Green : {green}</Text>
+
+            <View
+                style={{
+                    height: 150,
+                    width: 150,
+                    backgroundColor: `rgb(${red},${green},${blue})`
+                }}
+            />
         </View>
     );
 };
